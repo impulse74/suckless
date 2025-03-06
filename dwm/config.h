ebc9fa862d484b46f06d2164080d0d632ac22c8c
ace <potkanzjedolsnickers@gmail.com>
2025-03-06 22:18:30 +0100

    veri nice

--- dwm/config.h
+++ dwm/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "firamono:size=10" };
-static const char dmenufont[]       = "firamono:size=10";
+static const char *fonts[]          = { "firacode:size=10" };
+static const char dmenufont[]       = "firacode:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -58,9 +58,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *volpluscmd[] = { "amixer", "set", "Master", "5%+", NULL };
-static const char *voldowncmd[] = { "amixer", "set", "Master", "5%-", NULL };
-static const char *volmutecmd[] = { "bash", "-c", "amixer get Master | grep -q '\\[off\\]' && amixer set Master unmute || amixer set Master mute", NULL };
+static const char *volpluscmd[] = { "amixer", "set", "PCM", "5%+", NULL };
+static const char *voldowncmd[] = { "amixer", "set", "PCM", "5%-", NULL };
+static const char *volmutecmd[] = { "bash", "-c", "amixer get PCM | grep -q '\\[off\\]' && amixer set PCM unmute || amixer set PCM mute", NULL };
 
 #include <X11/XF86keysym.h>
 
